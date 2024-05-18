b36089c39cf751d32f699b4ddd8587ef76fd681d
hhana1-1 <hhana1@live.com>
2024-05-18 21:27:45 +0200

    s

--- Week1/world.js
+++ Week1/world.js
@@ -15,65 +15,27 @@ connection.connect((err) => {
   console.log('Connected to the database');
 });
 
-connection.query('SELECT Name FROM country WHERE Population > 8000000', (error, results) => {
-  if (error) throw error;
-  console.log('Countries with population greater than 8 million:');
-  console.log(results);
-});
+const executeQuery = (query, description) => {
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error(`Error executing query "${query}": ${error.stack}`);
+      return;
+    }
+    console.log(description);
+    console.log(results);
+  });
+};
 
-connection.query("SELECT Name FROM country WHERE Name LIKE '%land%'", (error, results) => {
-  if (error) throw error;
-  console.log('Countries with "land" in their names:');
-  console.log(results);
-});
-
-connection.query('SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000', (error, results) => {
-  if (error) throw error;
-  console.log('Cities with population between 500,000 and 1 million:');
-  console.log(results);
-});
-
-connection.query("SELECT Name FROM country WHERE Continent = 'Europe'", (error, results) => {
-  if (error) throw error;
-  console.log('Countries in Europe:');
-  console.log(results);
-});
-
-connection.query('SELECT Name FROM country ORDER BY SurfaceArea DESC', (error, results) => {
-  if (error) throw error;
-  console.log('Countries in descending order of surface area:');
-  console.log(results);
-});
-
-connection.query("SELECT Name FROM city WHERE CountryCode = 'NLD'", (error, results) => {
-  if (error) throw error;
-  console.log('Cities in the Netherlands:');
-  console.log(results);
-});
-
-connection.query("SELECT Population FROM city WHERE Name = 'Rotterdam'", (error, results) => {
-  if (error) throw error;
-  console.log('Population of Rotterdam:');
-  console.log(results);
-});
-
-connection.query('SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10', (error, results) => {
-  if (error) throw error;
-  console.log('Top 10 countries by surface area:');
-  console.log(results);
-});
-
-connection.query('SELECT Name FROM city ORDER BY Population DESC LIMIT 10', (error, results) => {
-  if (error) throw error;
-  console.log('Top 10 most populated cities:');
-  console.log(results);
-});
-
-connection.query('SELECT SUM(Population) AS TotalPopulation FROM country', (error, results) => {
-  if (error) throw error;
-  console.log('Population of the world:');
-  console.log(results);
-});
+executeQuery('SELECT Name FROM country WHERE Population > 8000000', 'Countries with population greater than 8 million:');
+executeQuery("SELECT Name FROM country WHERE Name LIKE '%land%'", 'Countries with "land" in their names:');
+executeQuery('SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000', 'Cities with population between 500,000 and 1 million:');
+executeQuery("SELECT Name FROM country WHERE Continent = 'Europe'", 'Countries in Europe:');
+executeQuery('SELECT Name FROM country ORDER BY SurfaceArea DESC', 'Countries in descending order of surface area:');
+executeQuery("SELECT Name FROM city WHERE CountryCode = 'NLD'", 'Cities in the Netherlands:');
+executeQuery("SELECT Population FROM city WHERE Name = 'Rotterdam'", 'Population of Rotterdam:');
+executeQuery('SELECT Name FROM country ORDER BY SurfaceArea DESC LIMIT 10', 'Top 10 countries by surface area:');
+executeQuery('SELECT Name FROM city ORDER BY Population DESC LIMIT 10', 'Top 10 most populated cities:');
+executeQuery('SELECT SUM(Population) AS TotalPopulation FROM country', 'Population of the world:');
 
 connection.end((err) => {
   if (err) {
