33186f6633e2eee51c7210d5dc8a4de6136eae3b
hhana1-1 <hhana1@live.com>
2024-05-21 22:16:53 +0200

    hmwerk

--- Week3/homework/mongodb/index.js
+++ Week3/homework/mongodb/index.js
@@ -67,10 +67,6 @@ const updateResult = await collection.updateOne({ season: "S30", episode: "E13" 
 }
 
 async function deleteEpisodeExercise(client) {
-  /**
-   * It seems an errand episode has gotten into our data.
-   * This is episode 14 in season 31. Please remove it and verify that it has been removed!
-   */
 
   const collection = client.db("databaseWeek3").collection("bob_ross_episodes");
   const deleteResult = await collection.deleteOne ({season: "S31", episode: "E14"});
@@ -119,16 +115,3 @@ async function main() {
 
 main();
 
-/**
- * In the end the console should read something like this: 
-
-Created season 9 episode 13 and the document got the id 625e9addd11e82a59aa9ff93
-The title of episode 2 in season 2 is WINTER SUN
-The season and episode number of the "BLACK RIVER" episode is S02E06
-The episodes that Bob Ross painted a CLIFF are NIGHT LIGHT, EVENING SEASCAPE, SURF'S UP, CLIFFSIDE, BY THE SEA, DEEP WILDERNESS HOME, CRIMSON TIDE, GRACEFUL WATERFALL
-The episodes that Bob Ross painted a CLIFF and a LIGHTHOUSE are NIGHT LIGHT
-Ran a command to update episode 13 in season 30 and it updated 1 episodes
-Ran a command to update all the BUSHES to BUSH and it updated 120 episodes
-Ran a command to delete episode and it deleted 1 episodes
- 
-*/
